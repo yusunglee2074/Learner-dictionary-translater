a1dc78bbe183b9211eb2e4a677f286554d637e42
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-12-23 00:51:21 +0900

    내가 만든 div 외에 클릭하면 창이 꺼진다

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // for status management button and translatedText
 let button = false;
-let text = false;
+let translated = false;
 
 // for share X, Y posiiton with 2 func
 let tempE;
 
 function highlightHandler(e) {
+  // 만약 내가 표시하고있는 Div영역 외의 곳을 클릭하면 모두 꺼져야된다.
+  if (e.target.className !== "yusunglee") {
+    translated = false;
+    if (document.getElementById('translatedText')) document.getElementById('translatedText').remove();
+  }
   // get the highlighted text
   let text = (document.all) ? document.selection.createRange().text : document.getSelection();
   // check if anything is actually highlighted
@@ -28,6 +33,7 @@ function doStuff(text, e) {
   let div = document.createElement('div');
   div.innerHTML = '<Button onClick="translateWithENEN()"><span style="font-size: 20px">번역</span></Button>';
   div.id = "yusunglee"
+  div.class = "yusunglee"
   div.style.position = "absolute"
   div.style.top = e.pageY + "px";
   div.style.left = e.pageX + "px";
@@ -41,13 +47,14 @@ function translateWithENEN() {
   document.getElementById('yusunglee').remove();
   button = false;
   let div = document.createElement('div');
-  div.innerHTML = '<span style="font-size: 20px">번역내용이 쭉 나올 것입니다. 하하하하하</span>';
+  div.innerHTML = `<span class="yusunglee" style="font-size: 20px">번역내용이 쭉 나올 것입니다. 하하하하하</span>`;
   div.id = "translatedText"
+  div.class = "yusunglee"
   div.style.position = "absolute"
   div.style.top = tempE.pageY + "px";
   div.style.left = tempE.pageX + "px";
-  if (!text) {
+  if (!translated) {
     document.body.appendChild(div);
-    text = true;
+    translated = true;
   }
 }
