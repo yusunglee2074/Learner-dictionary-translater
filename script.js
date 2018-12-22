92d9fc9c3e1f32b6440a4fd98246aa7621035216
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-12-22 16:49:53 +0900

    write popup.html and js contentscript and scritp.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function highlightHandler(e) {
+    // get the highlighted text
+    var text = document.getSelection();
+    // check if anything is actually highlighted
+    if(text !== '') {
+        // we've got a highlight, now do your stuff here
+        doStuff(text);
+    }
+}
+
+document.onmouseup = highlightHandler;
+
+
+function doStuff(text) {
+  window.alert("하이")
+}
+
