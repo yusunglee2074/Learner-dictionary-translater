acb972517d9bda06f9734802c93525d5f3da63e8
lee yusung <yusunglee2074@lees-MacBook-Pro.local>
2018-12-22 17:47:34 +0900

    when occur selection event show translate button

--- script.js
+++ script.js
@@ -4,16 +4,42 @@ function highlightHandler(e) {
     // check if anything is actually highlighted
     if(text.toString() !== '') {
         // we've got a highlight, now do your stuff here
-        doStuff(text);
+        doStuff(text, e);
     }
 }
 
 document.onmouseup = highlightHandler;
 if (!document.all) document.captureEvents(Event.MOUSEUP);
 
+let tempE;
 
-function doStuff(text) {
-  window.alert(text.toString())
-  console.log(text.toString())
+function doStuff(text, e) {
+  if (document.getElementById("translatedTextDiv")) {
+    document.getElementById('translatedTextDiv').remove()
+  }
+  if (document.getElementById("yusunglee")) {
+    document.getElementById('yusunglee').remove()
+  }
+  tempE = e;
+  let div = document.createElement('div');
+  div.innerHTML = '<Button onClick="translateWithENEN()"><span style="font-size: 20px">번역</span></Button>';
+  div.id = "yusunglee"
+  div.style.position = "absolute"
+  div.style.top = e.pageY + "px";
+  div.style.left = e.pageX + "px";
+  document.body.appendChild(div);
 }
 
+function translateWithENEN() {
+  document.getElementById('yusunglee').remove()
+  if (document.getElementById("translatedTextDiv")) {
+    document.getElementById('translatedTextDiv').remove()
+    let div = document.createElement('div');
+    div.innerHTML = '<span style="font-size: 20px">번역내용이 쭉 나올 것입니다. 하하하하하</span>';
+    div.id = "translatedTextDiv"
+    div.style.position = "absolute"
+    div.style.top = tempE.pageY + "px";
+    div.style.left = tempE.pageX + "px";
+    document.body.appendChild(div);
+  }
+}
